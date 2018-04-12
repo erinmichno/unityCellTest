7ca17cd910801a53a3adfa900c48573edc7428d1
ErinHughes <erinmichno@gmail.com>
2018-04-12 12:25:55 +0100

    initial

---
+++ Assets/CellScript.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellScript : MonoBehaviour {
+
+    //agent params
+    private Color color;
+    private CellScript parent;
+    private int O2Level;
+    //end agent params (position in transform)
+    private Material currentMaterial;
+
+    List<GameObject> SubcellList = new List<GameObject>();
+
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Spheroid.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[ExecuteInEditMode]
+public class Spheroid : MonoBehaviour {
+
+    public CellScript cellTemplate;
+    public int StartingCellNumber = 16;
+    List<CellScript> currentCells = new List<CellScript>();
+    // Use this for initialization
+    float currentRad = 1.01f;
+	void Start () {
+        currentCells.Clear();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        if(currentCells.Count == 0)
+        {
+            GenerateSomeCells();
+        }
+		
+	}
+
+    void GenerateSomeCells()
+    {
+        if(cellTemplate == null)
+        {
+            return;
+        }
+        for (int i = 0; i < StartingCellNumber; ++i)
+        {
+            Vector3 pos = cellTemplate.transform.position;
+            Vector3 offset = Random.onUnitSphere * currentRad;
+            CellScript cs = GameObject.Instantiate<CellScript>(cellTemplate, pos + offset, Quaternion.identity, this.transform);
+            currentCells.Add(cs);
+
+        }
+    }
+}
