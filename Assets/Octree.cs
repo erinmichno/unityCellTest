4b8f2a7584fcd6ac2a12891a1557b6f7c87b681a
ErinHughes <erinmichno@gmail.com>
2018-04-17 15:10:53 +0100

    simple octree visualisation

--- Assets/Octree.cs
+++ Assets/Octree.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -16,24 +17,95 @@ public enum OctreeIndex
 }
 
 
+public class OctreeNode<TType>
+{
+    Vector3 position; //pos of the cube
+    float size; //width/height/depth etc
+    OctreeNode<TType>[] subNodes;
+    List<TType> values; //leaf
+
+    public IEnumerable<OctreeNode<TType>> Nodes { get { return subNodes; } }
+
+    public Vector3 Position { get { return position; } }
+    public float Size { get { return size; } }
+
+    public OctreeNode(Vector3 pos, float size)
+    {
+        position = pos;
+        this.size = size;
+    }
+    public void Subdivide(int depth = 0)
+    {
+        subNodes = new OctreeNode<TType>[8];
+        for (int i = 0; i < subNodes.Length; ++i)
+        {
+            Vector3 newPos = position;
+            if ((i & 4) == 4)
+            {
+                newPos.y += size * 0.25f;
+            }
+            else
+            {
+                newPos.y -= size * 0.25f;
+            }
+
+            if ((i & 2) == 2)
+            {
+                newPos.x += size * 0.25f;
+            }
+            else
+            {
+                newPos.x -= size * 0.25f;
+            }
+
+            if ((i & 1) == 1)
+            {
+                newPos.z += size * 0.25f;
+            }
+            else
+            {
+                newPos.z -= size * 0.25f;
+            }
+
+            subNodes[i] = new OctreeNode<TType>(newPos, size * 0.5f);
+            if (depth > 0)
+            {
+                subNodes[i].Subdivide(depth - 1);
+            }
+        }
+    }
+
+    public bool IsLeaf()
+    {
+        return subNodes == null;
+    }
+}
+
 public class Octree<TType>
 {
     private OctreeNode<TType> node;
+
+    public OctreeNode<TType>  GetRoot()
+    {
+        return node;
+    }
+
     private int depth;
 
-    private class OctreeNode<TType>
+    public Octree(Vector3 position, float size, int depth)
     {
-        Vector3 position; //pos of the cube
-        float size; //width/height/depth etc
-        OctreeNode<TType>[] subNodes = new OctreeNode<TType>[8];
-        List<TType> values; //leaf
+       node = new OctreeNode<TType>(position, size);
+        node.Subdivide(depth);
+      
     }
+
+    
      private int GetIndexOfPosition(Vector3 lookupPosition, Vector3 nodePosition)
     {
         int index = 0;
-        index |= lookupPosition.y > nodePosition.y ? 0 : 4;
-        index |= lookupPosition.x > nodePosition.x ? 0 : 2;
-        index |= lookupPosition.z > nodePosition.z ? 0 : 1;
+        index |= lookupPosition.y > nodePosition.y ? 4 : 0;
+        index |= lookupPosition.x > nodePosition.x ? 2 : 0;
+        index |= lookupPosition.z > nodePosition.z ? 1 : 0;
         return index;
     }
 }
---
+++ Assets/OctreeComponent.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OctreeComponent : MonoBehaviour {
+
+    public float size;
+    public int depth;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnDrawGizmos()
+    {
+        Octree<int> octree = new Octree<int>(this.transform.position, size, depth);
+        
+        DrawNode(octree.GetRoot());
+    }
+
+    private Color minColor = new Color(1, 1.0f, 0, 1f);
+    private Color maxColor = new Color(0, 1.0f, 0.0f, 1.0f);
+    void DrawNode(OctreeNode<int> node, int nodeDepth = 0)
+    {
+        if (!node.IsLeaf())
+        {
+            foreach (var subnode in node.Nodes)
+            {
+                DrawNode(subnode, nodeDepth + 1);
+            }
+        }
+        Gizmos.color = Color.Lerp(new Color(1, 1.0f, 0, 1f), new Color(0, 1.0f, 0.0f, 1.0f), nodeDepth / (float)depth);
+        Gizmos.DrawWireCube(node.Position, Vector3.one * node.Size);
+
+
+    }
+}
