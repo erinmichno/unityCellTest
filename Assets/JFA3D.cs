6e8145e555be8bff2d5a46456d754f314faea649
DESKTOP-JQONVM0\Erin Hughes <erinmichno@gmail.com>
2018-05-02 15:15:45 +0100

    starting rt

--- Assets/JFA3D.cs
+++ Assets/JFA3D.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 struct DataType
 {
-    Vector3 pos;
+   public Vector3 pos;
 }
 
 
@@ -13,13 +13,34 @@ public class JFA3D : MonoBehaviour {
     const int seedCount = 32;
     DataType[] data = new DataType[seedCount];
     ComputeBuffer seedBuffer;
-	// Use this for initialization
-	void Start () {
+
+    RenderTexture rt;
+    int res = 128;
+
+    // Use this for initialization
+    void Start () {
 
         seedBuffer = new ComputeBuffer(data.Length, 3*sizeof(float));
         //init
+        for(int i = 0; i < data.Length; ++i)
+        {
+            data[i].pos = Random.insideUnitSphere * (res / 2);
+        }
+
         seedBuffer.SetData(data);
-	}
+
+
+
+        rt = new RenderTexture(res, res, 0, RenderTextureFormat.ARGBHalf, RenderTextureReadWrite.Linear);
+        rt.wrapMode = TextureWrapMode.Clamp;
+        rt.filterMode = FilterMode.Bilinear;//may need to be point
+        rt.enableRandomWrite = true;
+        rt.dimension = UnityEngine.Rendering.TextureDimension.Tex3D;
+        rt.volumeDepth = res;
+        rt.useMipMap = false;
+        rt.Create();
+
+    }
 	
 	// Update is called once per frame
 	void Update () {
